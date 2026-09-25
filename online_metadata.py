d5850bc7f24c0d514c189c6f9a48a79aa554e25d
Luqman234 <m-11835888@moe-dl.edu.my>
2026-09-25 21:31:41 +0800

    chore: update MusicBrainz client version to 0.17.0

--- online_metadata.py
+++ online_metadata.py
@@ -13,7 +13,7 @@ from pathlib import Path
 
 MUSICBRAINZ_API_ROOT = "https://musicbrainz.org/ws/2"
 DEFAULT_USER_AGENT = (
-    "MeowPlayer/0.16.4 "
+    "MeowPlayer/0.17.0 "
     "(https://github.com/Luqman234/MeowPlayer)"
 )
 
